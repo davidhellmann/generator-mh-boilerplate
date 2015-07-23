2ae6d1da2475c85626ad9ee6fdad6a8e7e4caa8b
Martin Herweg <m.herweg@conceptbakery.de>
2015-07-23 17:38:52 +0200

    some changes on the index.js

--- app/index.js
+++ app/index.js
@@ -5,27 +5,27 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
-module.exports = yeoman.generators.Base.extend({
+var mhBoilerplateGenerator = yeoman.generators.Base.extend({
 
   init: function() {
     this.pkg = require('../package.json');
   },
 
   askFor: function() {
+    var done = this.async();
+
     var prompts = [
       {
         type: 'input',
         name: 'projectName',
         message: 'Name the project (name of the theme folder in Wordpress)',
         default: 'mh-boilerplate'
-      },
-      {
+      },{
         type: 'input',
         name: 'projectDescription',
         message: 'Short description of the Project`',
         default: 'undefined'
-      },
-      {
+      },{
         type: 'list',
         name: 'projectUsage',
         message: 'Which purpose does this Project have? Choose the appropriate option',
@@ -35,32 +35,27 @@ module.exports = yeoman.generators.Base.extend({
           "Craft",
           "laravel"
         ]
-      },
-      {
+      },{
         type: 'input',
         name: 'projectVersion',
         message: 'Project Version Number',
         default: '0.0.1'
-      },
-      {
+      },{
         type: 'input',
         name: 'projectAuthor',
         message: 'Project Author or company',
         default: 'undefined'
-      },
-      {
+      },{
         type: 'input',
         name: 'projectMail',
         message: 'Mailadress of the author',
         default: 'undefined'
-      },
-      {
+      },{
         type: 'input',
         name: 'projectUrl',
         message: 'Author URl',
         default: 'http://...'
-      },
-      {
+      },{
         type: 'input',
         name: 'projectRepo',
         message: 'Git Repo URL',
@@ -68,7 +63,7 @@ module.exports = yeoman.generators.Base.extend({
       }
     ];
 
-    this.prompt(prompts, function() {
+    this.prompt(prompts, function(props) {
       this.projectName = props.projectName;
       this.projectDescription = props.projectDescription;
       this.projectUsage = props.projectUsage;
@@ -77,6 +72,7 @@ module.exports = yeoman.generators.Base.extend({
       this.projectMail = props.projectMail;
       this.projectUrl = props.projectUrl;
       this.projectRepo = props.projectRepo;
+      done();
     }.bind(this));
   },
 
@@ -90,7 +86,18 @@ module.exports = yeoman.generators.Base.extend({
 
   },
 
+  projectfiles: function() {
+    this.copy('_package.json', 'package.json');
+    this.copy('_bower.json', 'bower.json');
+    this.copy('_gulpfile.js', 'gulpfile.js');
+    this.copy('_gitignore', '.gitignore');
+    this.copy('editorconfig', '.editorconfig');
+    this.copy('jshintrc', '.jshintrc');
+  },
+
   install: function () {
-    this.installDependencies();
+    // this.installDependencies();
   }
 });
+
+module.exports = mhBoilerplateGenerator;
