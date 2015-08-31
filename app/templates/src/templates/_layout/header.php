611f260f853aea741014dbb96097f25e942cfb7f
Martin Herweg <m.herweg@conceptbakery.de>
2015-08-31 16:53:54 +0200

    removed title from header and added vertical rythm variable

--- app/templates/src/templates/_layout/header.php
+++ app/templates/src/templates/_layout/header.php
@@ -3,7 +3,7 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge,chrome=1">
-  <title>test</title>
+  <title></title>
   <script src="assets/js/vendor/modernizr-custom.min.js"></script>
   <link rel="stylesheet" href="assets/css/style.css">
 </head>
