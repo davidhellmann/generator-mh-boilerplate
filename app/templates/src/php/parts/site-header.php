f97c30ebb3a781113a7aa83299df2bcc7352af70
Martin Herweg <m.herweg@conceptbakery.de>
2015-12-08 16:52:01 +0100

    fixed a typo in the site-header.php

--- app/templates/src/php/parts/site-header.php
+++ app/templates/src/php/parts/site-header.php
@@ -5,10 +5,10 @@
     <meta http-equiv="X-UA-COMPATIBLE" content="IE=edge,chrome=1">
     <?php
         include "header/_favicons.php";
-        include "header/_metatgs.php";
+        include "header/_metatags.php";
     ?>
     <title></title>
-    {#<script src="assets/js/vendor/modernizr-custom.min.js"></script>#}
-    {#<link rel="stylesheet" href="assets/css/style.css">#}
+    <script src="assets/js/vendor/modernizr-custom.min.js"></script>
+    <link rel="stylesheet" href="assets/css/style.css">
 </head>
 <body>
