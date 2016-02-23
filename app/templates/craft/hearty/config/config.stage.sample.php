0856b82e133daf3fe6da6299622921c099b780fa
Martin Herweg <martin@martinherweg.de>
2016-02-23 15:04:46 +0100

    added sample config

---
+++ app/templates/craft/hearty/config/config.stage.sample.php
@@ -0,0 +1,34 @@
+<?php
+
+/*
+*	Config file: Local environment
+*
+*/
+
+return array(
+	'db' => array(
+		'server' => 'localhost',
+		'user' => 'user',
+		'password' => 'password',
+		'database' => 'database',
+	),
+
+	'siteUrl' => array(
+		'en' => CRAFT_SITE_URL . 'en',
+		'de' => CRAFT_SITE_URL . 'de'
+	),
+	'devMode' => false,
+	'phpMaxMemoryLimit' => '256M',
+    'backupDbOnUpdate' => true,
+    'translationDebugOutput' => false,
+    'useCompressedJs' => false,
+    'enableTemplateCaching' => false,
+    'userSessionDuration'           => 'P101Y',
+	'rememberedUserSessionDuration' => 'P101Y',
+	'rememberUsernameDuration'      => 'P101Y',
+	'defaultCpLanguage' => 'en',
+	'translationDebugOutput' => false,
+
+	//'testToEmailAddress'  => 'tester@testingtesting123.com',
+
+);
