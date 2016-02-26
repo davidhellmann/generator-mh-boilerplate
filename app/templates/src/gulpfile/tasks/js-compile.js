9dbb9e121d54010774bb00b89dccba8eedaaa16f
Martin Herweg <martin@martinherweg.de>
2016-02-26 14:32:53 +0100

    added js bundler and the compiler

---
+++ app/templates/src/gulpfile/tasks/js-compile.js
@@ -0,0 +1,17 @@
+/**
+   * compile and bundle js Files with browserify and babelify
+   **/
+import gulp from 'gulp';
+import bundleJs from '../lib/js-bundle';
+import bundleVar from '../lib/js-bundleVar';
+
+const compileJs = () => {
+    const bundler = bundleVar();
+
+    return bundleJs(bundler);
+
+    bundleJs(bundler);
+}
+
+gulp.task('compile:js', compileJs);
+module.exports = compileJs;
