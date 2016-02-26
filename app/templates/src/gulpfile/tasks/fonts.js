e9decfd5672fefba29f99976cd58f86f00cfe6bd
Martin Herweg <martin@martinherweg.de>
2016-02-26 16:23:21 +0100

    added fonts task

---
+++ app/templates/src/gulpfile/tasks/fonts.js
@@ -0,0 +1,18 @@
+/**
+  * Copy Font Files
+  **/
+import config from '../../config.json';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+
+const $ = gulpLoadPlugins()
+
+const copyFonts = () => {
+    return gulp
+        .src(config.src.src + config.src.fonts + '**/*')
+        .pipe($.changed(config.dist.dist + config.dist.fonts))
+        .pipe(gulp.dest(config.dist.dist + config.dist.fonts))
+}
+
+gulp.task('fonts', copyFonts);
+module.exports = copyFonts;
