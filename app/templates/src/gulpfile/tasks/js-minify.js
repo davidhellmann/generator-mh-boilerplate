55e98744c27f88e6c327fb4e2a98e90fc030abc8
Martin Herweg <martin@martinherweg.de>
2016-02-26 12:34:39 +0100

    add js minify task

---
+++ app/templates/src/gulpfile/tasks/js-minify.js
@@ -0,0 +1,27 @@
+/**
+   * Copy View files
+   **/
+import config from '../../config.json';
+import gulp from 'gulp';
+import gulpLoadPlugins from 'gulp-load-plugins';
+import yargs from 'yargs';
+
+const argv = yargs.argv;
+const $ = gulpLoadPlugins();
+
+const jsMinify = () => {
+    const env = argv.env || 'development';
+    return gulp
+        .src(config.dist.dist + config.dist.js + '**/*.js')
+        .pipe($.size({
+            title: 'JS Files Before'
+        }))
+        .pipe(argv.env == 'development' ? $.uglify() : $.util.noop())
+        .pipe(gulp.dest(config.dist + config.dist.js))
+        .pipe($.size({
+            title: 'JS Files after'
+        }))
+}
+
+gulp.task('minify:js', jsMinify);
+module.exports = jsMinify;
