f2be65443bc197a795b4e70b918ac538180c4dca
Martin Herweg <martin@martinherweg.de>
2016-05-25 16:41:24 +0200

    import lazysizes and picturefill to the app.js

--- app/templates/src/js/app.js
+++ app/templates/src/js/app.js
@@ -3,3 +3,7 @@
 // your partials can be in any other folder than 'json', 'my-source' or 'single'
 // these folders are used for other tasks
 // you can change the name of this file in the config.json
+
+
+import 'lazysizes'
+import 'picturefill'
