d1da80c9b8c7d07aec957d9d8aa8db83d300d7b9
Martin Herweg <martin@martinherweg.de>
2016-08-30 16:12:04 +0200

    deleted old stuff for complete rewrite

--- app/templates/src/gulpfile/tasks/css-minify.js
+++
@@ -1,22 +0,0 @@
-/**
-   * minify Css
-   **/
-import config from '../../config.json';
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import postCssNano from '../lib/postCssNano';
-
-const $ = gulpLoadPlugins();
-
-const minifyCss = () => {
-    return gulp
-        .src(config.dist.dist + config.dist.css + '**/*.css')
-        .pipe($.size({title: 'css before'}))
-        .pipe($.postcss(postCssNano()))
-        .pipe($.size({title: 'css after'}))
-            .pipe(gulp.dest(config.dist.dist + config.dist.css))
-        }
-
-    gulp.task('minify:css', minifyCss);
-    module.exports = minifyCss;
-
--- app/templates/src/gulpfile/tasks/images-copy.js
+++
@@ -1,16 +0,0 @@
-/**
-  * Copy Images
-  **/
-import config from '../../config.json';
-import gulp from 'gulp';
-import gulpLoadPlugins from 'gulp-load-plugins';
-import copyImages from '../lib/images-copy';
-
-const $ = gulpLoadPlugins();
-
-const imagesTask = () => {
-    return copyImages(config.src.src + config.src.images.base + '**/*.{png,jpeg,jpg,gif,webp,PNG,JPEG,JPG,GIF}', config.dist.dist + config.dist.images.base)
-}
-
-gulp.task('copy:images', imagesTask);
-module.exports = imagesTask;
