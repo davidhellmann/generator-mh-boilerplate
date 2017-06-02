8535e48a09dcaac2c619f2643e35c1f07bda89e0
Martin Herweg <martin@martinherweg.de>
2017-06-02 16:04:48 +0200

    added a promptMessage function and used it in generalPrompts

--- generators/app/modules/prompt-modules/_general.js
+++ generators/app/modules/prompt-modules/_general.js
@@ -1,21 +1,93 @@
 'use strict';
+const chalk = require('chalk');
+
+// Importing message helper function
+const message = require('../../helpers/promptMessage');
+
 const generalPrompts = [
   {
     type: 'input',
     name: 'projectName',
-    message: 'Please provide a Project Name (without-spaces)'
+    message: message({
+      headline: 'Project Name',
+      description: 'Please provide a Project Name (without-spaces)',
+      defaultValue: 'Current Name'
+    }),
+    default: function () {
+      return process.cwd().split('/').pop(-1).toLowerCase().replace(/\s/g, '');
+    },
+    validate: function (input) {
+      // Do async stuff
+      if (input.indexOf(' ') >= 0 || /[~`!#$%^&*+=[\]\\';,/{}|\\":<>?]/g.test(input)) {
+        // Pass the return value in the done callback
+        console.log('\n' + chalk.styles.red.open + 'No whitespaces or special-chars allowed!' + chalk.styles.red.close);
+        return false;
+      }
+      return true;
+    }
+  },
+  {
+    type: 'input',
+    name: 'projectDescription',
+    message: message({
+      headline: 'Project Description',
+      description: 'Please provide a description for your project.',
+      defaultValue: false
+    })
+  },
+  {
+    type: 'input',
+    name: 'projectVersion',
+    message: message({
+      headline: 'Project Version',
+      description: 'Define Project Version'
+    }),
+    default: '0.0.1'
+  },
+  {
+    type: 'input',
+    name: 'projectProxy',
+    message: message({
+      headline: 'Project Local Domain',
+      description: 'Define Project Local Domain, defaults to your project name'
+    }),
+    default: function (answers) {
+      // If the
+      if (answers.projectName.includes('.dev') || answers.projectName.includes('.local')) {
+        return answers.projectName;
+      }
+      return `${answers.projectName}.dev`;
+    }
   },
   {
     type: 'input',
     name: 'authorName',
-    message: 'Your Name',
+    message: message({
+      headline: 'Author Name',
+      description: 'Please provide your name',
+      defaultValue: false
+    }),
     store: true
   },
   {
     type: 'input',
     name: 'authorEmail',
-    message: 'Your Email',
+    message: message({
+      headline: 'Author E-Mail',
+      description: 'Please provide your e-mail',
+      defaultValue: false
+    }),
     store: true
+  },
+  {
+    type: 'input',
+    name: 'authorHomepage',
+    message: message({
+      headline: 'Author Homepage',
+      description: 'Note the Author Homepage',
+      defaultValue: false,
+      store: true
+    })
   }
 ];
 
--- generators/app/modules/writing-modules/_package.json.js
+++ generators/app/modules/writing-modules/_package.json.js
@@ -4,10 +4,13 @@ const extend = require('deep-extend');
 function basePackageJson(files = {}) {
   extend(files.pkg, {
     name: this.props.projectName,
+    description: this.props.projectDescription,
+    version: this.props.projectVersion,
     authors: [
       {
         name: this.props.authorName,
-        email: this.props.authorEmail
+        email: this.props.authorEmail,
+        homepage: this.props.authorHomepage
       }
     ]
   });
