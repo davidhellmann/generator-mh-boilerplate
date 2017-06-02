c98c9919c901525c5ba79648c78d3e9d5fab3cba
Martin Herweg <martin@martinherweg.de>
2017-06-02 16:04:18 +0200

    added more tests for basic config.json and package.json information

--- __tests__/app.js
+++ __tests__/app.js
@@ -2,32 +2,56 @@
 var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
+const fs = require('fs'); // eslint-disable-line no-unused-vars
 
 // Define some variables
-const projectName = 'boilerplate-test';
+const project = {
+  name: 'boilerplate-test',
+  description: 'A small Test Driven Generator',
+  version: '0.0.1',
+  proxy: 'boilerplate-test.dev'
+};
 const author = {
   name: 'Martin Herweg',
-  email: 'martin@herweg.co'
+  email: 'martin@herweg.co',
+  homepage: 'https://martinherweg.de'
 };
 
 const run = () => helpers.run(path.join(__dirname, '../generators/app'));
 
 describe('mh-boilerplate', () => {
+  beforeAll(() => {
+    return run()
+      .withPrompts({
+        projectName: project.name,
+        projectVersion: project.version,
+        projectDescription: project.description,
+        projectProxy: project.proxy,
+        authorName: author.name,
+        authorEmail: author.email,
+        authorHomepage: author.homepage
+      });
+  });
   // Test for Basic Files
   describe('Basic Files and Infos', () => {
-    beforeAll(() => {
-      return run()
-        .withPrompts({
-          projectName,
-          authorName: author.name,
-          authorEmail: author.email
-        });
-    });
     // Test package.json content
     it('fill package.json with correct Information', () => {
       assert.JSONFileContent('package.json', { // eslint-disable-line new-cap
-        name: projectName,
-        authors: [{name: author.name, email: author.email}]
+        name: project.name,
+        description: project.description,
+        version: project.version,
+        authors: [{name: author.name, email: author.email, homepage: author.homepage}]
+      });
+    });
+
+    // Fills config.json
+    it('fills config.json with information', () => {
+      assert.JSONFileContent('config.json', { // eslint-disable-line new-cap
+        proxy: project.proxy,
+        favicon: {
+          developerURL: author.homepage,
+          developerName: author.name
+        }
       });
     });
   });
