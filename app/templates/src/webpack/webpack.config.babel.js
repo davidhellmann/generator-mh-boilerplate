aebd3c6f68e9d84b3e62796109280e4ffe4080e3
Martin Herweg <martin@martinherweg.de>
2017-06-02 09:28:34 +0200

    added sourcemap option to postcss-loader

--- app/templates/src/webpack/webpack.config.babel.js
+++ app/templates/src/webpack/webpack.config.babel.js
@@ -199,6 +199,9 @@ export default {
             },
             {
               loader: 'postcss-loader',
+              options: {
+                sourceMap: true,
+              },
             },
             {
               loader: 'sass-loader',
