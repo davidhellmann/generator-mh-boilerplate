0b65c3184f49923a880d6751f2f625aaa7296a41
Martin Herweg <martin@martinherweg.de>
2017-06-04 16:46:47 +0200

    added helper for download files

---
+++ generators/app/helpers/_downloadFiles.js
@@ -0,0 +1,33 @@
+const download = require('download');
+const ProgressBar = require('progress');
+const commentLog = require('./_logComment');
+
+const bar = new ProgressBar('[:bar] :percent :etas', {
+  complete: '=',
+  incomplete: ' ',
+  width: 20,
+  total: 0
+});
+
+const downloadFiles = ({url, destination} = {}) => {
+  commentLog({
+    message: 'Downloading',
+    color: 'green',
+    short: true
+  });
+
+  return download(url, destination, {
+    extract: true,
+    mode: '775'
+  }).on('response', res => {
+    bar.total = res.headers['content-length'];
+    res.on('data', data => bar.tick(data.length));
+  }).then(() => commentLog({
+    message: 'Finished Download',
+    color: 'green',
+    short: true
+  }))
+    .catch(error => console.error(error));
+};
+
+module.exports = downloadFiles;
