864ecd1f8a48b9f48fd931297a080acc6109d453
Martin Herweg <martin@martinherweg.de>
2017-06-10 19:47:37 +0200

    readded babel preset for webpack config

--- generators/app/modules/packageJson-modules/devDependencies/_javascript.js
+++ generators/app/modules/packageJson-modules/devDependencies/_javascript.js
@@ -19,6 +19,7 @@ const babel = {
   'babel-plugin-syntax-dynamic-import': '^6.18.0',
   'babel-plugin-transform-runtime': '^6.23.0',
   'babel-preset-env': '^1.4.0',
+  'babel-preset-es2015': '^6.24.1',
   'babel-polyfill': '^6.23.0',
   'babel-register': '^6.23.0',
   'babel-runtime': '^6.23.0'
