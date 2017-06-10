f4ce0eeb4d6aba171a752b6b662c96db7b3372da
Martin Herweg <martin@martinherweg.de>
2017-06-10 14:34:13 +0200

    changed babel config

--- generators/app/modules/packageJson-modules/devDependencies/_javascript.js
+++ generators/app/modules/packageJson-modules/devDependencies/_javascript.js
@@ -18,8 +18,8 @@ const babel = {
   'babel-plugin-lodash': '^3.2.11',
   'babel-plugin-syntax-dynamic-import': '^6.18.0',
   'babel-plugin-transform-runtime': '^6.23.0',
-  'babel-preset-es2015': '^6.22.0',
-  'babel-preset-stage-2': '^6.22.0',
+  'babel-preset-env': '^1.4.0',
+  'babel-polyfill': '^6.23.0',
   'babel-register': '^6.23.0',
   'babel-runtime': '^6.23.0'
 };
