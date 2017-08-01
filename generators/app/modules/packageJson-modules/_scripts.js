079296322ca5814bccd34bbec82bb9b46550c63e
Martin Herweg <martin@martinherweg.de>
2017-08-02 00:56:03 +0200

    added commands in scripts in package.json

--- generators/app/modules/packageJson-modules/_scripts.js
+++ generators/app/modules/packageJson-modules/_scripts.js
@@ -9,13 +9,14 @@
 const extend = require('deep-extend');
 
 exports.scripts = {
-  init_commands: 'cross-env NODE_ENV=production gulp build:production --env=development --changed=false && cross-env NODE_ENV=production webpack --hide-modules --config=webpack/webpack.config.babel.js',
-  init: 'npm run -s init_commands',
-  dev: 'cross-env NODE_ENV=development webpack-dashboard -p 3002 -- gulp --env=browser-sync',
-  'dev:single': 'cross-env NODE_ENV=development gulp --env=browser-sync',
-  production: 'cross-env NODE_ENV=production gulp build:production --env=production && cross-env NODE_ENV=production webpack --hide-modules --config=webpack/webpack.config.babel.js -p',
+  'dev:dashboard': 'cross-env NODE_ENV=development webpack-dashboard -p 3002 -- gulp --env=browser-sync',
+  dev: 'cross-env NODE_ENV=development gulp --env=browser-sync',
   gulp: 'cross-env NODE_ENV=development gulp',
   images: 'cross-env NODE_ENV=development gulp move:images',
+  init: 'npm run -s init_commands && node ./scripts/createDirs.js',
+  init_commands: 'cross-env NODE_ENV=development gulp build:production --env=browserSync --changed=false && cross-env NODE_ENV=development webpack --hide-modules --config=webpack/webpack.config.babel.js',
+  module: 'node ./scripts/module',
+  production: 'cross-env NODE_ENV=production gulp build:production --env=production && cross-env NODE_ENV=production webpack --hide-modules --config=webpack/webpack.config.babel.js -p',
   webpack: 'cross-env NODE_ENV=production webpack --hide-modules --config=webpack/webpack.config.babel.js',
   'webpack:analyze': 'webpack-bundle-analyzer webpack/stats.json dist/public/'
 };
