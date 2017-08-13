8210d8f0bdf34f0e665a6c401fab96e99f288581
Martin Herweg <martin@martinherweg.de>
2017-08-13 14:22:51 +0200

    refactored some of the modules creation code

--- generators/app/templates/scripts/module.js
+++ generators/app/templates/scripts/module.js
@@ -12,49 +12,58 @@ const inquirer = require('inquirer');
 const path = require('path');
 const pkg = require('../package.json');
 
-
-function createModule({moduleName, components = {} }) {
+function createModule({ moduleName, components = {} }) {
   var store = memFs.create();
   var fs = editor.create(store);
-  const srcPath = path.resolve(pkg.srcPaths.views);
+  const srcPath = path.resolve(`${pkg.srcPaths.views}modules`);
   const vuePath = `${path.resolve(pkg.srcPaths.js)}`;
-  const dist = {
-    path: moduleName.includes('/') ? moduleName.split('/')[0] + '/' : '',
-    name: moduleName.includes('/') ? '_' + moduleName.split('/')[1] : `${moduleName}/_${moduleName}`,
-    template: moduleName.includes('/') ? moduleName.split('/')[1] : `${moduleName}`,
-  };
-  const fileName = dist.path + dist.name;
-  const fileExtension = <% if (projectUsage == 'craft') { -%>
-  '.html'
-  <% } else if (projectUsage == 'laravel') { -%>
-  '.blade.php'
-  <% } %>
+
+  const basePath = path.resolve(__dirname, '../');
+
+  let moduleArray = moduleName.split('/');
+  let dist = {};
+  dist.name = moduleArray[moduleArray.length - 1].toLowerCase();
+  dist.path = moduleArray.length > 1 ? moduleArray.slice(0, -1).join('/') : `${dist.name}`;
+  const fileName = path.resolve(srcPath, `${dist.path}/${dist.name}`);
+
+  let fileExtension;
+
+  switch (pkg.projectType) {
+    case 'craft':
+      fileExtension = '.html';
+      break;
+    case 'laravel':
+      fileExtension = '.blade.php';
+
+    default:
+      break;
+  }
+
   try {
     if (components.js) {
-      fs.copyTpl(path.resolve(__dirname, './moduleTemplates/_script.js'), `${srcPath}/modules/${fileName}.scripts.js`, {
-        moduleName: dist.template,
+      fs.copyTpl(path.resolve(__dirname, './moduleTemplates/_script.js'), `${fileName}.scripts.js`, {
+        moduleName: dist.name,
       });
-      console.log(`${srcPath}/modules/${fileName}.scripts.js`);
+      console.log(`${fileName}.scripts.js`);
     }
 
-
     if (components.css) {
-      fs.copyTpl(path.resolve(__dirname, './moduleTemplates/_style.scss'), `${srcPath}/modules/${fileName}.styles.scss`, {
-        moduleName: dist.template,
+      fs.copyTpl(path.resolve(__dirname, './moduleTemplates/_style.scss'), `${fileName}.styles.scss`, {
+        moduleName: dist.name,
       });
-      console.log(`${srcPath}/modules/${fileName}.styles.scss`);
+      console.log(`${fileName}.styles.scss`);
     }
 
     if (components.template) {
-      fs.copyTpl(path.resolve(__dirname, './moduleTemplates/_template.html'), `${srcPath}/modules/${fileName}-template${fileExtension}`, {
-        moduleName: dist.template,
+      fs.copyTpl(path.resolve(__dirname, './moduleTemplates/_template.html'), `${fileName}-template${fileExtension}`, {
+        moduleName: dist.name,
       });
-      console.log(`${srcPath}/modules/${fileName}-template${fileExtension}`);
+      console.log(`${fileName}-template${fileExtension}`);
     }
 
     if (components.vue) {
       fs.copyTpl(path.resolve(__dirname, './moduleTemplates/_template.vue'), path.resolve(`${vuePath}/${moduleName}.vue`), {
-        moduleName: dist.template,
+        moduleName: dist.name,
       });
       console.log(`${vuePath}/${moduleName}.vue`);
     }
@@ -63,18 +72,20 @@ function createModule({moduleName, components = {} }) {
     fs.commit(function(done) {
       console.log('done');
     });
-  } catch(e) { console.error(e); }
+  } catch (e) {
+    console.error(e);
+  }
 }
 
-if(argv._.length) {
+if (argv._.length) {
   return createModule({
     moduleName: argv._[0],
     components: {
       js: argv.js || argv.javascript,
       css: argv.css || argv.scss || argv.style,
       template: argv.html || argv.template,
-      vue: argv.vue
-    }
+      vue: argv.vue,
+    },
   });
 }
 
@@ -93,7 +104,7 @@ inquirer
       choices: [
         {
           name: 'Style',
-          value: 'css'
+          value: 'css',
         },
         {
           name: 'JavaScript',
@@ -101,18 +112,18 @@ inquirer
         },
         {
           name: 'Template',
-          value: 'template'
-        }
-      ]
-    }
+          value: 'template',
+        },
+      ],
+    },
   ])
-  .then(async (answers) => {
+  .then(async answers => {
     createModule({
       moduleName: answers.moduleName,
       components: {
         js: answers.components.includes('js') ? true : false,
         css: answers.components.includes('css') ? true : false,
-        template: answers.components.includes('template') ? true : false
-      }
+        template: answers.components.includes('template') ? true : false,
+      },
     });
   });
